d18d3b6a2998b86e08e41b5019408cc696651917
tejaby <yostintejaxun20@gmail.com>
2023-10-18 17:31:34 -0600

    add new fields to CustomUser model

--- backend/apps/user/models.py
+++ backend/apps/user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser
+
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    passport_number = models.CharField(max_length=20, blank=True, null=True)
+    nationality  = models.CharField(max_length=50, blank=True, null=True)
+    date_of_birth  = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuarios'
+
+    def __str__(self):
+        return f'{self.username}'
