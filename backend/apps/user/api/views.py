7e3cdb60cd69485d8f06aaf7866691867c01898b
tejaby <yostintejaxun20@gmail.com>
2023-10-19 01:45:24 -0600

    improvement and comments

--- backend/apps/user/api/views.py
+++ backend/apps/user/api/views.py
@@ -8,20 +8,39 @@ from apps.user.models import CustomUser
 # serializers
 from .serializers import UserSerializer, UserListSerializer
 
+
+"""
+Vista basada en funcion para creacion y listado de usuarios
+
+- En el metodo get se utiliza el serializer UserListSerializer y en post UserSerializer
+
+"""
+
+
 @api_view(['GET', 'POST'])
 def user_api_view(request):
     if request.method == 'GET':
-        user = CustomUser.objects.filter(is_active = True).values('id', 'username', 'first_name', 'last_name', 'email', 'date_joined', 'passport_number', 'nationality', 'date_of_birth')
+        user = CustomUser.objects.filter(is_active=True).values(
+            'id', 'username', 'first_name', 'last_name', 'email', 'date_joined', 'passport_number', 'nationality', 'date_of_birth')
         user_serializer = UserListSerializer(user, many=True)
         return Response(user_serializer.data, status=HTTP_200_OK)
-    
+
     elif request.method == 'POST':
         user_serializer = UserSerializer(data=request.data)
         if user_serializer.is_valid():
             user_serializer.save()
-            return Response(user_serializer.data, status=HTTP_201_CREATED)
+            return Response({
+                'message': 'Usuario creado exitosamente',
+                'user': user_serializer.data}, status=HTTP_201_CREATED)
         return Response(user_serializer.errors)
-    
+
+
+"""
+Vista basada en funcion para obtener detalles de usuario
+
+"""
+
+
 @api_view(['GET'])
 def user_detail_api_view(request, pk):
     if request.method == 'GET':
