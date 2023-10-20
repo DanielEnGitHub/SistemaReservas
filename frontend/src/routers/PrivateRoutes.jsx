31ed4730dd623595844ca0aca5c7744a10bfd8fc
tejaby <yostintejaxun20@gmail.com>
2023-10-19 23:14:13 -0600

    add logout, removed token

--- frontend/src/routers/PrivateRoutes.jsx
+++ frontend/src/routers/PrivateRoutes.jsx
@@ -1,8 +1,38 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Title from "../components/Texts/Title";
 
+import { Button } from "@chakra-ui/react";
+
+// services
+import { logoutService } from "../services/user";
+
+// context
+import { UserContext } from "../context/User";
+
+// hooks
+import { useTokenLocalStorage } from "../hooks/userTokenLocalStorage";
+
+// react
+import { useContext } from "react";
+
 const PrivateRoutes = () => {
+  const { setUser, setToken } = useContext(UserContext);
+
+  const { getToken, removeToken } = useTokenLocalStorage("token");
+
+  const token = getToken();
+
+  const handleLogout = async () => {
+    const data = {
+      token: token,
+    };
+    await logoutService(data);
+    setUser(null);
+    setToken(null);
+    removeToken();
+  };
+
   /* RUTAS PRIVADAS */
   return (
     <>
@@ -34,7 +64,28 @@ const PrivateRoutes = () => {
         <Route
           end
           path="/products"
-          element={<Title content="products" black />}
+          element={
+            <>
+              <Title content="products" black />
+              <Button colorScheme="red" onClick={handleLogout}>
+                logout
+              </Button>
+              <Link to={"/app/test/"}>test</Link>
+            </>
+          }
+        />
+        <Route
+          end
+          path="/test"
+          element={
+            <>
+              <Title content="test" black />
+              <Button colorScheme="red" onClick={handleLogout}>
+                logout
+              </Button>
+              <Link to={"/app/products/"}>products</Link>
+            </>
+          }
         />
 
         <Route path="*" element={<h2>404</h2>} />
